8646c422cfc74cceb8ee79f0b33a737989b1d067
Yvann <34337152+MrHD586@users.noreply.github.com>
2020-03-24 20:40:24 +0100

    Edit deploy files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
 lock "~> 3.12.0"
 
 set :application, "picture-galleries"
-set :repo_url, "https://https://github.com/Vidivanze/CLD2-Picture-Galleries.git"
+set :repo_url, "https://github.com/Vidivanze/CLD2-Picture-Galleries.git"
